56f595050fc0aa5adf20401ee969f2f66c7a132f
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2025-10-13 20:40:48 +0530

    datatypes

---
+++ UnIt_1/datatypes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=5;
+
+    float b=2.5;
+
+    char c='x';
+
+
+    printf("the value of a=%d\n",a);
+    printf("the value of b=%f\n",b);
+    printf("the value of c=%c\n",c);
+
+    return 0;
+}
