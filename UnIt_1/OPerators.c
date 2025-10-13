b73ee2adee4c1c16811e695d7124cd3466945f6b
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2025-10-13 20:40:02 +0530

    operator

---
+++ UnIt_1/OPerators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a=4;
+    int b=5;
+    int c=a+b;
+    printf("a=%d,b=%d,Sum=%d\n",a,b,c);
+    //Modulus operator is used to get remainder
+    printf("%d\n",a%b);
+    return 0;
+
+
+}
