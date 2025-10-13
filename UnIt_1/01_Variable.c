5b8ebdc47765e518dae9d8fe608c43d471e094d6
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2025-10-13 22:46:53 +0530

    some bsic and short programs

---
+++ UnIt_1/01_Variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 10; // declRE nd intialise variable
+    int j = i;
+    // %d is called format specifier
+
+    // % d is for int and %f for float and %c is for char
+
+    printf("the value of i =%d\n the value of j= %d\n",i,j);
+    return 0;
+}
---
+++ UnIt_1/multiVariable.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int a=2,b=4,c=7,d=5 ;
+    //declaring multiple variable 
+    int x =a+b-c%d;
+    printf("a=%d\n,b=%d\n,c=%d\n,d=%d\n ,x%d",a,b,c,d,x);
+    return 0;
+}
