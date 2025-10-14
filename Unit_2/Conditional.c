3ab1731897dfefd48c41ce35337a7bf8b761c45d
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2025-10-14 23:10:23 +0530

    example of conditional statement

---
+++ Unit_2/Conditional.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int age = 15;
+
+    if(age>10){
+        printf("We are inside if\n");
+        printf("Your age is greater than 10\n");
+    }
+    if(age%5==0){
+        printf("We are inside another if\n");
+        printf("Your age is divisible by 50\n");
+    }
+    return 0;
+}
+
+/*We are inside if
+Your age is greater than 10
+We are inside another if
+Your age is divisible by 50*/
